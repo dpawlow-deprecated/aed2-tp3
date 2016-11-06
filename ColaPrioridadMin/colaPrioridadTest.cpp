b6b9b68462baca28da38c5259fbd8d5252ba2ded
Dante <dante.pawlow@gmail.com>
2016-11-06 18:14:23 -0300

    Agrego makefile y estructura de tests para colaPrioridad

---
+++ ColaPrioridadMin/colaPrioridadTest.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "mini_test.h"
+
+#include "colaPrioridadMin.hpp"
+
+//void test_punteros_cola_vacia(){
+//    ColaPrioridad<int> c;
+
+//    ASSERT(c.raiz_ == NULL);
+//    ASSERT(c.padreParaAgregar_ == NULL);
+
+//    c.encolar(2);
+//    ASSERT(c.raiz_->valor == 2);
+//    ASSERT(c.raiz_->padre == NULL);
+//    ASSERT(c.padreParaAgregar_->valor == NULL);
+//}
+
+int main() {
+    //RUN_TEST(test_punteros_cola_vacia);
+
+
+    return 0;
+}
