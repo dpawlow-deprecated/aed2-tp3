aa89854b43fe845304f437fe011d6471f74a9cc4
Dario Ocles <dario.ocles@gmail.com>
2016-11-20 18:56:59 -0300

    Agregando Cola de los tad basicos

---
+++ Cola.h
@@ -0,0 +1,56 @@
+#ifndef COLA_H_
+#define COLA_H_
+
+#include "Modulos-CPP/aed2/Vector.h"
+
+using namespace std;
+using namespace aed2;
+
+template<typename T>
+class Cola {
+  public:
+      Cola(); // Vacia()
+      void Encolar(T& elem);
+      bool EsVacia() const;
+      T Proximo() const;
+      T Desencolar();
+      Nat Tamanio() const;
+
+  private:
+      Vector<T> _elementos;
+};
+
+
+template<typename T>
+Cola<T>::Cola() {
+    _elementos = Vector<T>();
+}
+
+template<typename T>
+void Cola<T>::Encolar(T& elem) {
+    _elementos.AgregarAtras(elem);
+}
+
+template<typename T>
+bool Cola<T>::EsVacia() const {
+    return _elementos.Longitud() == 0;
+}
+
+template<typename T>
+T Cola<T>::Proximo() const {
+    return _elementos.Ultimo();
+}
+
+template<typename T>
+T Cola<T>::Desencolar() {
+    T aux = _elementos.Ultimo();
+    _elementos.TirarUltimos(1);
+    return aux;
+}
+
+template<typename T>
+Nat Cola<T>::Tamanio() const {
+    return _elementos.Longitud();
+}
+
+#endif
---
+++ test_cola.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <fstream>
+#include "Cola.h"
+#include "mini_test.h"
+
+using namespace std;
+using namespace aed2;
+
+
+void test_crearcola() {
+    Cola<Nat> c;
+    ASSERT(c.EsVacia());
+    ASSERT(c.Tamanio() == 0);
+}
+
+void test_5_elementos() {
+    Cola<Nat> c;
+    ASSERT(c.EsVacia());
+
+    Nat t1 = 10;
+    c.Encolar(t1);
+    ASSERT(c.EsVacia() == false);
+    ASSERT(c.Proximo() == 10);
+    ASSERT(c.Tamanio() == 1);
+
+    Nat t2 = 11;
+    c.Encolar(t2);
+    ASSERT(c.EsVacia() == false);
+    ASSERT(c.Proximo() == 11);
+    ASSERT(c.Tamanio() == 2);
+
+    Nat t3 = 12;
+    c.Encolar(t3);
+    ASSERT(c.EsVacia() == false);
+    ASSERT(c.Proximo() == 12);
+    ASSERT(c.Tamanio() == 3);
+
+    Nat t4 = 13;
+    c.Encolar(t4);
+    ASSERT(c.EsVacia() == false);
+    ASSERT(c.Proximo() == 13);
+    ASSERT(c.Tamanio() == 4);
+
+    Nat t5 = 14;
+    c.Encolar(t5);
+    ASSERT(c.EsVacia() == false);
+    ASSERT(c.Proximo() == 14);
+    ASSERT(c.Tamanio() == 5);
+
+
+    c.Desencolar();
+    ASSERT(c.Proximo() == 13);
+
+    c.Desencolar();
+    ASSERT(c.Proximo() == 12);
+
+    c.Desencolar();
+    ASSERT(c.Proximo() == 11);
+
+    c.Desencolar();
+    ASSERT(c.Proximo() == 10);
+
+    c.Desencolar();
+
+    ASSERT(c.Tamanio() == 0);
+    ASSERT(c.EsVacia());
+}
+
+void test_muchas_cosas() {
+
+    Cola<Nat> c;
+
+    for (int i = 1; i <= 200; i++) {
+        Nat e = i;
+        c.Encolar(e);
+        ASSERT(c.Proximo() == e);
+        ASSERT(c.Tamanio() == i);
+        ASSERT(c.EsVacia() == false);
+    }
+
+    Nat n = 200;
+    while (c.EsVacia() == false) {
+        ASSERT(c.Proximo() == n);
+        ASSERT(c.Tamanio() == n);
+        c.Desencolar();
+        n--;
+    }
+
+    ASSERT(c.Tamanio() == 0);
+    ASSERT(c.EsVacia());
+}
+
+
+int test_cola() {
+	RUN_TEST(test_crearcola);
+	RUN_TEST(test_5_elementos);
+	RUN_TEST(test_muchas_cosas);
+    return 0;
+}
+
+int main() {
+	return test_cola();
+}
